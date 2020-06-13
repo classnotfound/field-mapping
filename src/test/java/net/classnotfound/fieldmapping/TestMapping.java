bb0b3e1d8bec4ccd52afe9e2234e6f6614634adf
herve <herve.roussel@classnotfound.net>
2020-06-13 10:14:48 +0200

    small refactoring to use constants in tests

--- src/test/java/net/classnotfound/fieldmapping/TestMapping.java
+++ src/test/java/net/classnotfound/fieldmapping/TestMapping.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.Test;
 
 public class TestMapping {
 
+    public static final String ADDINFO = "addinfo";
+    public static final String INFO = "info";
+
     @Test
     public void testSuccess() {
-        Foo bar = Foo.builder().info("info").build();
+        Foo bar = Foo.builder().info(INFO).build();
         FooDto foo = FooMapper.INSTANCE.toDto(bar);
         Assertions.assertEquals("info", foo.getInfo());
     }
@@ -16,7 +19,7 @@ public class TestMapping {
     public void testFailed() {
         Foo bar = Foo.builder().addInfo("addinfo").build();
         FooDto foo = FooMapper.INSTANCE.toDto(bar);
-        Assertions.assertEquals("addinfo", foo.getAddInfo());
+        Assertions.assertEquals(ADDINFO, foo.getAddInfo());
     }
 
 }
